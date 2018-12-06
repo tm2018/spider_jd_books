acbdbc6e11bb3a618a0bd55a662b014ebc6e9648
bobo <1744381765@qq.com>
2018-12-07 04:47:46 +0800

    测试

--- functions/get_book_nav_info.py
+++ functions/get_book_nav_info.py
@@ -12,7 +12,7 @@ book_nav_html = getJdBookUrlFun(jd_book_url).decode("gbk").encode("utf-8")
 
 def getBookNav1():
         #编写正则表达式
-        pattern = re.compile(r'?</i><a>(.*)</a>')
+        pattern = re.compile(r'?</i><a>(.* )</a>')
         # print  book_nav_html
         book_nav1_name = pattern.findall(book_nav_html)
         print book_nav1_name
