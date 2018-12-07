5b00cd22951d05504c7b13ec4461cb2effde8fe8
duxiaobo <1744381765@qq.com>
2018-12-07 18:16:59 +0800

    啊啊啊

--- functions/get_book_nav_info.py
+++ functions/get_book_nav_info.py
@@ -6,15 +6,17 @@ from open_jd import getJdBookUrlFun
 获取书籍一级分类名字&url、二级分类名字&url
 """
 
-jd_book_url = "https://book.jd.com/"
+jd_book_sort_url = "https://book.jd.com/booksort.html"
 
-book_nav_html = getJdBookUrlFun(jd_book_url).decode("gbk").encode("utf-8")
+book_nav_html = getJdBookUrlFun(jd_book_sort_url).decode("gbk").encode("utf-8")
 
 def getBookNav1():
         #编写正则表达式
-        pattern = re.compile(r'?</i><a>(.* )</a>')
+        pattern = re.compile(r'<dt><a href="(.*?)">(.*?)</dt><dd><em><a href="(.*?)">(.*?)</a></em>')
         # print  book_nav_html
         book_nav1_name = pattern.findall(book_nav_html)
         print book_nav1_name
+        for i in book_nav1_name:
+                print i[0],i[1]
 getBookNav1()
 
--- functions/open_jd.py
+++ functions/open_jd.py
@@ -6,8 +6,8 @@ import urllib2
 # sys.setdefaultencoding('utf-8')
 
 #定义url
-jd_main_url = "https://www.jd.com/"
 jd_book_url = "https://book.jd.com/"
+jd_book_sort_url = "https://book.jd.com/booksort.html"
 
 #定义一个函数假装访问jd主页
 def getJdUrlFun(url):
@@ -18,18 +18,18 @@ def getJdUrlFun(url):
         res_data = urllib2.urlopen(req,timeout=10)
         res = res_data.read()
         res_data.close()
-        print "我在假装访问京东主页"
+        print "我在假装访问京东图书主页"
         return res
 
 #定义一个函数访问京东图书
-def getJdBookUrlFun(url):
+def getJdBookUrlFun(url=""):
     #假装访问京东主页
-    getJdUrlFun(jd_main_url)
+    getJdUrlFun(jd_book_url)
     headers = {
         "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
-        "referer": "https://www.jd.com/"
+        "referer": "https://book.jd.com/"
     }
-    req = urllib2.Request(url, headers=headers)
+    req = urllib2.Request(jd_book_sort_url, headers=headers)
     res_data = urllib2.urlopen(req, timeout=10)
     res = res_data.read()
     res_data.close()
