7a9e018ee6ddd4bdd23d31aa7eff7c53dbd766d0
bobo <1744381765@qq.com>
2018-12-16 14:10:44 +0800

    selenium

--- functions/get_book_nav2_info.py
+++ functions/get_book_nav2_info.py
@@ -13,7 +13,7 @@ sys.setdefaultencoding('utf-8')
 type = sys.getfilesystemencoding()
 #将网页以utf-8格式解析然后转换为系统默认格式
 
-
+count = 1
 
 #处理下获取到的url_str字符串，返回最终访问京东的合适的url
 def combine_url(url_str):
@@ -25,31 +25,27 @@ def combine_url(url_str):
     url = "%s?cat=%s&tid=%s" %('https://list.jd.com/list.html',cat,tid)
     return url
 
-b = getBookNav()
-for i in b:
-        for k in i:
-                # print k,i[k]
-                urls = map(combine_url,i[k])
-                # print urls
 
-a ='https://list.jd.com/list.html?cat=1713,3285,3760&tid=3760'
-# print a
 
 #根据html取出下一页地址和提取书籍相关信息
 def get_nav2_book_info(url):
         html = getNav2HtmlFun(url)
+        print url
         tree = etree.HTML(html)
         next_page_list = tree.xpath('//*[@id="J_bottomPage"]/span[@class="p-num"]/a[@class="pn-next"]/@href')
         #图书信息可以点击“更多”进去查看，bookmore_url是他的url
-        bookmore_url = tree.xpath('//*[@id="plist"]/ul/li/div/div[@class="p-name"]/a/@href')
+        bookmore_url_list = tree.xpath('//*[@id="plist"]/ul/li/div/div[@class="p-name"]/a/@href')
         #把url组合起来
-        bookmore_spider_url = "%s%s" %("https:",bookmore_url[0])
+        for bookmore_url in bookmore_url_list:
+            #轮询url_list，组合成可访问的url
+            bookmore_spider_url = "%s%s" %("https:",bookmore_url)
         #这里访问书籍信息时，假装从当前的url跳转过去，所以以当前url伪装成referer
-        book_html = getBookHtml(bookmore_spider_url)
-        print bookmore_spider_url
+            book_info = getBookHtml(bookmore_spider_url)
+            print "当前访问的bookmore_spider_url:%s" %bookmore_spider_url
+            for k,v in book_info.items():
+                print k,":",v
 
-        book_name = tree.xpath("//*[@id='jd-price']/text()")
-        print book_name
+
         try:
             if next_page_list[0]:
                     next_page = "%s%s" %("https://list.jd.com",next_page_list[0])
@@ -58,7 +54,20 @@ def get_nav2_book_info(url):
                     print "next_page is null!"
         except Exception as e:
                     print "there is an error when get next_page!error_info:%s" %e
-get_nav2_book_info('https://list.jd.com/list.html?cat=1713,3285,3760&tid=3760')
+# get_nav2_book_info('https://list.jd.com/list.html?cat=1713,3285,3760&tid=3760')
+
+b = getBookNav()
+for i in b:
+        for k in i:
+                # print k,i[k]
+                urls = map(combine_url,i[k])
+                # print urls
+                for i in urls:
+                    count1 = count + 1
+                    print "这是第%s次访问" %(count1)
+                    get_nav2_book_info(i)
+
+a ='https://list.jd.com/list.html?cat=1713,3285,3760&tid=3760'
 
 
 
--- functions/spider_jd.py
+++ functions/spider_jd.py
@@ -9,6 +9,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+#计数：访问book_info的次数
+
 
 #定义url
 jd_book_url = "https://book.jd.com/"
@@ -55,17 +57,34 @@ def getNav2HtmlFun(url=""):
 # print getJdBookUrlFun(jd_book_url).decode("gbk").encode("utf-8")
 
 #用selenium.webdriver模拟浏览器访问，获取相关信息
+count = 1
 def getBookHtml(url):
-    # desired_capabilities = DesiredCapabilities.CHROME  # 修改页面加载策略
-    # desired_capabilities["pageLoadStrategy"] = "none"  # 这个值表示只加载html，不加载动态获取的内容如js ajax等
     browser = webdriver.Chrome()
     browser.get(url)
-    # browser.set_page_load_timeout(1)
-    #让webdriver等待，直到获取到了价格
-    WebDriverWait(browser, 0).until(EC.presence_of_element_located((By.XPATH, '//strong[@id="jd-price"]')))
-    # browser.switch_to.frame(frame)
-    price = browser.find_element_by_xpath('//strong[@id="jd-price"]')
-    print price.text
+    #try...catch尝试取访问并且取值，如果遇到异常则把各种值设置为0并输入url
+    try:
+        # 让webdriver等待，直到获取到了价格
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//strong[@id="jd-price"]')))
+        book_name = browser.find_element_by_xpath('//*[@id ="name"]/div[@class="sku-name"]').text
+        jd_price = browser.find_element_by_xpath('//strong[@id="jd-price"]').text
+        pricing = browser.find_element_by_xpath('//*[@id="page_maprice"]').text
+        publishing_house = browser.find_element_by_xpath('//*[@id="parameter2"]/li[@clstag="shangpin|keycount|product|chubanshe_3"]/a').text
+    except Exception as e:
+        print "this is an error when get book_info:%s" %e
+        book_name = 0
+        jd_price =0
+        pricing = 0
+        publishing_house =0
+        pass
+    book_info = {
+            "book_name": book_name,
+            "jd_price": jd_price,
+            "pricing": pricing,
+            "publishing_house": publishing_house
+    }
+    browser.quit() #模拟访问完一个就关闭掉一个
+
+    return book_info
 
 
 
